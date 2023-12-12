e33fb845f4f70c1458bcb7385822d2b206f1338b
Shattik <1905008@ugrad.cse.buet.ac.bd>
2023-12-12 08:41:16 +0600

    cubeDone

--- Offline1/magic_cube.cpp
+++ Offline1/magic_cube.cpp
@@ -17,6 +17,124 @@
 using namespace std;
 
 int rotAngle = 0, rotInc = 10;
+double factor = 1;
+int sectorCount = 20;
+double cylAngle = 70.5287794;
+double maxRad = 1/sqrt(3);
+double curRad = 0;
+double incRad = maxRad/10;
+
+void drawSpherePart()
+{
+
+    int parts = 50;
+    for(int i=0; i<parts; i++){
+        double x1 = 1 - 2.0*i/parts;
+        double x2 = 1 - 2.0*(i+1)/parts;
+        for(int j=0; j<=parts; j++){
+            double y1 = 1 - 2.0*j/parts;
+            double y2 = 1 - 2.0*(j+1)/parts;
+            Vector v1(x1, y1, 1);
+            Vector v2(x2, y1, 1);
+            Vector v3(x2, y2, 1);
+            Vector v4(x1, y2, 1);
+            v1.normalize();
+            v2.normalize();
+            v3.normalize();
+            v4.normalize();
+            v1 = v1 * curRad;
+            v2 = v2 * curRad;
+            v3 = v3 * curRad;
+            v4 = v4 * curRad;
+            drawQuad(v1.x, v1.y, v1.z, v2.x, v2.y, v2.z, v3.x, v3.y, v3.z, v4.x, v4.y, v4.z);
+        }
+    }
+}
+
+void colorSphere(int i)
+{
+    if(i%3 == 0){
+        glColor3f(1, 0, 0);
+    }
+    else if(i%3 == 1){
+        glColor3f(0, 1, 0);
+    }
+    else{
+        glColor3f(0, 0, 1);
+    }
+}
+
+void drawSphere()
+{
+    int num = 0;
+    for(int i=0; i<4; i++){
+        glPushMatrix();
+        glRotatef(90*i, 1, 0, 0);
+        glTranslatef(0, 0, factor);
+        colorSphere(num);
+        num = (num+1)%2;
+        drawSpherePart();
+        glPopMatrix();
+    }
+    for(int i=-1; i<2; i+=2){
+        glPushMatrix();
+        glRotatef(90*(-i), 0, 1, 0);
+        glTranslatef(0, 0, factor);
+        colorSphere(2);
+        drawSpherePart();
+        glPopMatrix();
+    }
+}
+
+void drawCylinder()
+{
+    double height = sqrt(2)*factor;
+
+    for(int i=0; i<sectorCount; i++){
+        double angle1 = cylAngle/2 - i*cylAngle/sectorCount;
+        double x1 = curRad*cos(rad(angle1));
+        double y1 = curRad*sin(rad(angle1));
+        double z1 = height/2;
+
+        double angle2 = cylAngle/2 - (i+1)*cylAngle/sectorCount;
+        double x2 = curRad*cos(rad(angle2));
+        double y2 = curRad*sin(rad(angle2));
+        double z2 = height/2;
+
+        drawQuad(x1, y1, -z1, x2, y2, -z2, x2, y2, z2, x1, y1, z1);
+    }
+}
+
+void drawCylinders()
+{
+    glColor3f(1, 1, 0);
+    double r = factor/sqrt(2);
+    for(int i=0; i<4; i++){
+        glPushMatrix();
+        glRotatef(90*i, 0, 0, 1);
+        glRotatef(45, 0, 1, 0);
+        glTranslatef(r, 0, 0);
+        drawCylinder();
+        glPopMatrix();
+    }
+    for(int i=0; i<4; i++){
+        glPushMatrix();
+        glRotatef(90*i, 0, 0, 1);
+        glRotatef(-45, 0, 1, 0);
+        glTranslatef(r, 0, 0);
+        drawCylinder();
+        glPopMatrix();
+    }
+    for(int i=0; i<4; i++){
+        glPushMatrix();
+        glRotatef(90*i, 0, 0, 1);
+        glRotatef(90, 1, 0, 0);
+        glRotatef(45, 0, 1, 0);
+        glTranslatef(r, 0, 0);
+        drawCylinder();
+        glPopMatrix();
+    }
+}
 
 void drawOctahedron()
 {
@@ -33,6 +151,9 @@ void drawOctahedron()
             }
             glPushMatrix();
             glRotatef(90*j, 0, 0, 1);
+            double shift = (1-factor)/3;
+            glTranslatef(shift, shift, shift);
+            glScalef(factor, factor, factor);
             drawTriangle(1, 0, 0, 0, 1, 0, 0, 0, 1);
             glPopMatrix();
             color = !color;
@@ -53,6 +174,8 @@ void display()
     glPushMatrix();
     glRotatef(rotAngle, 0, 0, 1);
     drawOctahedron();
+    drawCylinders();
+    drawSphere();
     glPopMatrix();
 
     glFlush();
@@ -91,6 +214,18 @@ void keyboardListener(unsigned char key, int x, int y)
             break;
         case 'd':
             rotAngle = (rotAngle + rotInc) % 360;
+            break;
+        case '.':
+            if(factor < 1){
+                factor += 0.1;
+                curRad -= incRad;
+            }
+            break;
+        case ',':
+            if(factor >= 0.1){
+                factor -= 0.1;
+                curRad += incRad;
+            }
             break;
         default:
             break;
